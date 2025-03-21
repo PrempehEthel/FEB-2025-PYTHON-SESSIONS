59f66ae91d8b1ada6a6b0729ebbd7cb4423ad513
PrempehEthel <ethelprempeh48@gmail.com>
2025-03-21 17:45:46 +0000

    Python week 2 assignment

---
+++ WEEk 2/assignment_2.py
@@ -0,0 +1,23 @@
+# Creating an empty list
+my_list = []
+
+# Appending the elements to my list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Inserting the value 15 at the second position
+my_list.insert(1, 15)
+
+# Extending the list with another list [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Removing the last element from my list
+my_list.pop()
+
+# Sorting my list in ascending order
+my_list.sort()
+
+# Findig and printing the index of the value 30 in my_list
+print(my_list.index(30))
